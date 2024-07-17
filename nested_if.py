17f22d76fff67bb89a48234a56b2b234f1ec57a1
Roger-Block <rjmblock@gmail.com>
2024-07-17 14:29:59 -0700

    updated

--- nested_if.py
+++ nested_if.py
@@ -1,3 +1,23 @@
+#========== 1. Nested Decisions: The Adventure Game ==========
+
+place = input("Choose a place: forest or cave? ")
+
+if place == "forest":
+    action = input("climb a tree or cross a river?")
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    else:
+        print("You found a boat!")
+elif place == "cave":
+    action = input("light a torch or proceed in the dark?")
+    if action == "light a torch":
+        print ("You awoken the bear!!! RUN!!!")
+    elif action == "proceed in the dark":
+        print ("You fell down a deep hole!")
+    else:
+        pass
+
+
 #======= 2. Quick Decisions: The Event Planner ================
 
 
